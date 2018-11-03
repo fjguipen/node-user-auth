168d6acffbcf7a6a458c078a4e773e3e48b22db4
fjguipen <fjguipen@gmail.com>
2018-11-03 21:18:25 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.get('/', (req, res)=>{
+app.get('/hola', (req, res)=>{
     res.send('Hello World')
+});
+
+app.get('/', (req, res)=>{
+    res.send('World')
 });
 
 app.listen(3000,"192.168.1.193", ()=>{
     console.log('Servidor escuchando');
-});localStorage
+});
